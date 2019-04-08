cda1cee64a8fe8b3f87ffcdb347f9c6765557e56
justdj <977878634@qq.com>
2019-04-08 21:24:21 +0800

    基础测试

---
+++ base/src/Test.java
@@ -0,0 +1,36 @@
+/*
+  Created by IntelliJ IDEA.
+  User: shan
+  Date: 19.4.8
+  Time: 21:20
+  Info:
+*/
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author justdj
+ * @email top90982@gmail.com
+ * Date: 19.4.8
+ * Time: 21:20
+ */
+public class Test {
+	
+	public static void main (String[] args){
+		
+		Integer a = 127;
+		Integer b = 127;
+		System.out.println(a == b);
+		
+		
+		String s = "string";
+		String t = new String("string");
+		System.out.println(s == t);
+		
+		
+		System.out.println(Math.round(-2.5));
+		
+		
+	}
+	
+}
